9a78450d464d7eb37968da9d2b9bce2b2c193ccf
Asya'ri ‎ <asyari818@gmail.com>
2023-01-30 17:08:12 +0700

    Create API Get User by Id

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -1,9 +1,11 @@
 const regiter = require("./register");
 const login = require("./login");
 const update = require("./update");
+const getUser = require("./getUser");
 
 module.exports = {
   regiter,
   login,
   update,
+  getUser,
 };
