eebfe3813e1e0646cbb711e83eedcfbe8e58d239
Asya'ri ‎ <asyari818@gmail.com>
2023-02-05 11:10:51 +0700

    Create API Get All User

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ router.post('/register', usersHandler.regiter);
 router.post('/login', usersHandler.login);
 router.put('/:id', usersHandler.update);
 router.get('/:id', usersHandler.getUser);
+router.get('/', usersHandler.getUsers);
 router.post('/logout', usersHandler.logout);
 
 module.exports = router;
